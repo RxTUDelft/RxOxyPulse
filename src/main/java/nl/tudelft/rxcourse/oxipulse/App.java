86183e9dcb5118b2f8a38bf8c9fedf56232f2ffc
Ishan Sital <irsital@gmail.com>
2014-04-17 11:50:37 +0200

    Initial commit after maven create

---
+++ src/main/java/nl/tudelft/rxcourse/oxipulse/App.java
@@ -0,0 +1,13 @@
+package nl.tudelft.rxcourse.oxipulse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
